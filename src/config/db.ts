08109b1d8ed44971271a43de063f9ee6d99be967
Andre Lima <andressj6@gmail.com>
2019-10-08 17:43:18 +0200

    db initialization

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,10 +2,15 @@ import mongoose from 'mongoose'
 
 const connectDb = async () => {
     mongoose.set('useCreateIndex', true)
-    await mongoose.connect('mongodb://localhost:27017/movies', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    try {
+        await mongoose.connect('mongodb://localhost:27017/movies', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+    } catch (e) {
+        console.log(e) // tslint:disable-line
+        process.exit()
+    }
 }
 
 export default connectDb
